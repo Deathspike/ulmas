e13d3e4c869d164406ef77fb6ce43115365774bb
Deathspike <meister.deathspike@outlook.com>
2022-06-02 12:50:37 +0200

    Fix missed view notation improvement

--- src/client/routes/series/main/views/SeriesView.tsx
+++ src/client/routes/series/main/views/SeriesView.tsx
@@ -2,12 +2,10 @@ import * as app from '..';
 import * as React from 'react';
 import * as ui from 'client/ui';
 
-export function SeriesView(props: {vm: app.SeriesViewModel}) {
-  return (
-    <ui.ImageLinkView title={props.vm.source.title} 
-      imageHeight={21} imageUrl={props.vm.posterUrl}
-      onClick={() => props.vm.open()}>
-      <ui.WatchView value={props.vm.source.unwatchedCount ?? 0} />
-    </ui.ImageLinkView>
-  );
-}
+export const SeriesView = ui.createView<{vm: app.SeriesViewModel}>(props => (
+  <ui.ImageLinkView title={props.vm.source.title} 
+    imageHeight={21} imageUrl={props.vm.posterUrl}
+    onClick={() => props.vm.open()}>
+    <ui.WatchView value={props.vm.source.unwatchedCount ?? 0} />
+  </ui.ImageLinkView>
+));
