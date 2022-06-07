91efb71a387be781d3ec9b9b7c79205bb863b898
Deathspike <meister.deathspike@outlook.com>
2022-06-07 20:46:37 +0200

    Fix keys causing clicks through screen loader

--- src/client/core/services/InputService.ts
+++ src/client/core/services/InputService.ts
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import {core} from 'client/core';
 
 export class InputService {
   constructor() {
     document.addEventListener('keydown', ev => {
-      if (!this.onDocumentKey(ev.code.toLowerCase())) return;
+      if (core.screen.waitCount || !this.handleKey(ev.code.toLowerCase())) return;
       ev.preventDefault();
       ev.stopPropagation();
     });
@@ -19,7 +20,7 @@ export class InputService {
 
   keyDown(handler: (keyName: string) => boolean) {
     return (ev: React.KeyboardEvent) => {
-      if (!handler(ev.code.toLowerCase())) return;
+      if (core.screen.waitCount || !handler(ev.code.toLowerCase())) return;
       ev.preventDefault();
       ev.stopPropagation();
     };
@@ -28,31 +29,31 @@ export class InputService {
   keyRestore() {
     const keys = ['enter', 'space'];
     return (ev: React.KeyboardEvent) => {
-      if (!keys.includes(ev.code.toLowerCase())) return;
+      if (core.screen.waitCount || !keys.includes(ev.code.toLowerCase())) return;
       ev.stopPropagation();
     };
   }
   
-  private onDocumentKey(keyName: string) {
+  private handleKey(keyName: string) {
     switch (keyName) {
       case 'arrowleft':
-        this.handleNavigation(-1, 0);
+        this.process(-1, 0);
         return true;
       case 'arrowright':
-        this.handleNavigation(1, 0);
+        this.process(1, 0);
         return true;
       case 'arrowdown':
-        this.handleNavigation(0, 1);
+        this.process(0, 1);
         return true;
       case 'arrowup':
-        this.handleNavigation(0, -1);
+        this.process(0, -1);
         return true;
       default:
         return false;
     }
   }
 
-  private handleNavigation(dirX: number, dirY: number) {
+  private process(dirX: number, dirY: number) {
     const current = document.activeElement && document.activeElement !== document.body
       ? fetchBox(document.activeElement)
       : undefined;
@@ -60,7 +61,7 @@ export class InputService {
       .map(fetchBox)
       .filter(x => x.index >= 0);
     const validElements = current && current.index >= 0
-      ? elements.filter(createFilter(dirX, dirY, current)).sort(createSorter(dirX, current))
+      ? elements.filter(createFilter(current, dirX, dirY)).sort(createSorter(current, dirX))
       : elements.sort((a, b) => a.index - b.index);
     const bestElement = validElements
       .map(x => x.element)
@@ -70,7 +71,7 @@ export class InputService {
   }
 }
 
-function createFilter(dirX: number, dirY: number, current: ReturnType<typeof fetchBox>) {
+function createFilter(current: ReturnType<typeof fetchBox>, dirX: number, dirY: number) {
   return (x: ReturnType<typeof fetchBox>) => {
     if (dirX < 0 && current.x - x.x <= 0) return false;
     if (dirX > 0 && current.x - x.x >= 0) return false;
@@ -80,7 +81,7 @@ function createFilter(dirX: number, dirY: number, current: ReturnType<typeof fet
   };
 }
 
-function createSorter(dirX: number, current: ReturnType<typeof fetchBox>) {
+function createSorter(current: ReturnType<typeof fetchBox>, dirX: number) {
   return (a: ReturnType<typeof fetchBox>, b: ReturnType<typeof fetchBox>) => {
     const ax = Math.abs(current.x - a.x);
     const ay = Math.abs(current.y - a.y);
