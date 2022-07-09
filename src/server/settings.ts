e1730e6daa6dd501ae7b851eefe785ac9ada7127
Deathspike <meister.deathspike@outlook.com>
2022-07-09 09:23:50 +0200

    Remove cache dependence on package version

--- src/server/settings.ts
+++ src/server/settings.ts
@@ -3,6 +3,6 @@ import path from 'path';
 const packageData = require('../../package');
 
 export const settings = {
-  cache: path.join(os.homedir(), packageData.name, packageData.version),
+  cache: path.join(os.homedir(), packageData.name, 'cache.v1'),
   sections: path.join(os.homedir(), packageData.name, 'sections.json')
 };
