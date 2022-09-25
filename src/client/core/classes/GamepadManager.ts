7e842ff2b7267bdf46747f3a621527f5577945de
Deathspike <meister.deathspike@outlook.com>
2022-09-25 16:52:01 +0200

    Fix gamepad handling in background

--- src/client/core/classes/GamepadManager.ts
+++ src/client/core/classes/GamepadManager.ts
@@ -2,12 +2,15 @@ import * as core from 'client/core';
 
 export class GamepadManager {
   private readonly gamepads = new Map<number, core.GamepadObserver>();
+  private hasFocus = true;
   private tickInterval?: NodeJS.Timer;
 
   static createEmulator() {
     const manager = new GamepadManager();
+    window.addEventListener('blur', () => manager.hasFocus = false);
     window.addEventListener('gamepadconnected', x => manager.connect(x.gamepad.index));
     window.addEventListener('gamepaddisconnected', x => manager.disconnect(x.gamepad.index));
+    window.addEventListener('focus', () => manager.hasFocus = true);
   }
 
   connect(index: number) {
@@ -24,16 +27,18 @@ export class GamepadManager {
   }
 
   private onTick() {
-    const activeElement = document.activeElement && document.activeElement !== document.body
-      ? document.activeElement
-      : document.documentElement;
-    for (const gamepad of this.gamepads.values()) {
-      const buttons = Array.from(gamepad.buttons());
-      const keyNames = buttons.map(translateButton);
-      for (const keyName of keyNames) {
-        activeElement.dispatchEvent(keyName === 'enter'
-          ? new MouseEvent('click', {bubbles: true, cancelable: true})
-          : new KeyboardEvent('keydown', {bubbles: true, cancelable: true, code: keyName}));
+    if (this.hasFocus) {
+      const activeElement = document.activeElement && document.activeElement !== document.body
+        ? document.activeElement
+        : document.documentElement;
+      for (const gamepad of this.gamepads.values()) {
+        const buttons = Array.from(gamepad.buttons());
+        const keyNames = buttons.map(translateButton);
+        for (const keyName of keyNames) {
+          activeElement.dispatchEvent(keyName === 'enter'
+            ? new MouseEvent('click', {bubbles: true, cancelable: true})
+            : new KeyboardEvent('keydown', {bubbles: true, cancelable: true, code: keyName}));
+        }
       }
     }
   }
