70b2c64be898f7d3b355d2a16de95918c3b41091
Deathspike <meister.deathspike@outlook.com>
2022-10-30 11:31:14 +0100

    Fix empty series showing in finished/unseen filter

--- src/api/functions.ts
+++ src/api/functions.ts
@@ -4,11 +4,11 @@ import {SeriesEntry} from './models';
 export function filterBy(a: MovieEntry | SeriesEntry, filter: 'all' | 'finished' | 'ongoing' | 'unseen') {
   switch (filter) {
     case 'finished':
-      return Boolean(a instanceof MovieEntry ? a.watched : !a.unwatchedCount);
+      return Boolean(a instanceof MovieEntry ? a.watched : a.totalCount && !a.unwatchedCount);
     case 'ongoing':
-      return Boolean(a instanceof MovieEntry ? a.resume : a.unwatchedCount !== a.totalCount && a.unwatchedCount);
+      return Boolean(a instanceof MovieEntry ? a.resume : a.unwatchedCount && a.unwatchedCount !== a.totalCount);
     case 'unseen':
-      return Boolean(a instanceof MovieEntry ? !a.watched : a.unwatchedCount === a.totalCount);
+      return Boolean(a instanceof MovieEntry ? !a.watched : a.totalCount && a.unwatchedCount === a.totalCount);
     default:
       return true;
   }
@@ -17,9 +17,9 @@ export function filterBy(a: MovieEntry | SeriesEntry, filter: 'all' | 'finished'
 export function sortBy(a: MovieEntry | SeriesEntry, b: MovieEntry | SeriesEntry, sort: 'dateAdded' | 'lastPlayed' | 'premieredDate' | 'title'): number {
   switch (sort) {
     case 'dateAdded':
-      const aa = a instanceof MovieEntry ? a.dateAdded : a.dateEpisodeAdded ?? a.dateAdded;
-      const ba = b instanceof MovieEntry ? b.dateAdded : b.dateEpisodeAdded ?? b.dateAdded
-      return aa.localeCompare(ba) || sortBy(a, b, 'title');
+      const ada = a instanceof MovieEntry ? a.dateAdded : a.dateEpisodeAdded ?? a.dateAdded;
+      const bda = b instanceof MovieEntry ? b.dateAdded : b.dateEpisodeAdded ?? b.dateAdded;
+      return ada.localeCompare(bda) || sortBy(a, b, 'title');
     case 'lastPlayed':
       if (a.lastPlayed && b.lastPlayed) return a.lastPlayed.localeCompare(b.lastPlayed);
       if (a.lastPlayed || b.lastPlayed) return a.lastPlayed ? 1 : -1;
